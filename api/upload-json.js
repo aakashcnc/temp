128a62dfe160b31210d4b0c4abbed9d8809a5c79
Aakash <aakash.cnc@gmail.com>
2024-07-17 21:12:08 +0530

    Update upload-json.js

--- api/upload-json.js
+++ api/upload-json.js
@@ -17,8 +17,7 @@ module.exports = async (req, res) => {
   }
 
   try {
-   const file = "json-data/home.json";
-   const fileBuffer = Buffer.from(await file.arrayBuffer());
+   
     const result = await put(`json-data/home.json`, {
       data: JSON.stringify(content), // Convert the content object to a JSON string
       contentType: 'application/json',
