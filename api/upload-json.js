86ece33cade7f3e61b7f2e444add2cfee39d83e9
Aakash <aakash.cnc@gmail.com>
2024-07-17 20:03:02 +0530

    Update upload-json.js

--- api/upload-json.js
+++ api/upload-json.js
@@ -1,7 +1,7 @@
 const { put } = require('@vercel/blob');
 
 module.exports = async (req, res) => {
-  if (req.method !== 'GET') {
+  if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
