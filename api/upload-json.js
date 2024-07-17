71f7344d6352c76eb8a574867da54da5027238b9
Aakash <aakash.cnc@gmail.com>
2024-07-17 20:51:07 +0530

    Update upload-json.js

--- api/upload-json.js
+++ api/upload-json.js
@@ -15,16 +15,4 @@ module.exports = async (req, res) => {
     return res.status(400).json({ error: 'Slug and content are required' });
   }
 
-  try {
-    const data = JSON.stringify(content, null, 2); // Convert the content object to a JSON string
-    const result = await put(`json-data/${slug}.json`, {
-      data: data,
-      contentType: 'application/json',
-    });
-
-    return res.status(200).json({ message: 'File uploaded successfully', url: result.url });
-  } catch (error) {
-    console.error('Error uploading file:', error);
-    return res.status(500).json({ error: error.message });
-  }
 };
