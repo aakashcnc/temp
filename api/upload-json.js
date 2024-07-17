8b58ba084879f6147319535b99ac42d9ec1e2db6
Aakash <aakash.cnc@gmail.com>
2024-07-17 21:03:12 +0530

    Update upload-json.js

--- api/upload-json.js
+++ api/upload-json.js
@@ -5,17 +5,27 @@ module.exports = async (req, res) => {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  const { slug, content } = req.body;
+  //const { slug, content } = req.body;
+ const { content } = {
+  name: "Sammy",
+  email: "sammy@example.com",
+  plan: "Pro"
+};
 
-
-
-  if (!slug || !content) {
+  if (!content) {
     return res.status(400).json({ error: 'Slug and content are required' });
   }
-   return res.status(200).json({ 
-      message: 'File uploaded successfully', 
-      slug: slug, 
-      content: content 
+
+  try {
+    const fileBuffer = Buffer.from(await file.arrayBuffer());
+    const result = await put(`json-data/home.json`, {
+      data: JSON.stringify(content), // Convert the content object to a JSON string
+      contentType: 'application/json',
     });
 
+    return res.status(200).json({ message: 'File uploaded successfully', url: result.url });
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    return res.status(500).json({ error: error.message });
+  }
 };
