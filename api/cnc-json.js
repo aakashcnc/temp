1b5f1784bc81fa6e2c8b1f52b271f32e35d4fc20
Aakash <aakash.cnc@gmail.com>
2024-07-18 15:39:48 +0530

    Update cnc-json.js

--- api/cnc-json.js
+++ api/cnc-json.js
@@ -3,13 +3,14 @@ const express = require('express');
 const axios = require('axios');
 const fs = require('fs').promises;
 const path = require('path');
+const app = express();
 require('dotenv').config();
 
 module.exports = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
-
+  const JSON_URL = 'https://microsoftedge.github.io/Demos/json-dummy-data/64KB.json';
   const content = JSON.stringify({
     name: "Sammy",
     email: "sammy@example.com",
